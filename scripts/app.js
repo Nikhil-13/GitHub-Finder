b7468d8eb40577db80767ef1edb1327084084a3d
Nikhil-13 <nnik270@gmail.com>
2023-07-12 22:07:00 +0530

    added debouncing and fixed the folder structure

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+const searchUser = document.querySelector('#user-name')
+const git = new GitHub()
+const ui = new UI()
+// let timerId
+
+searchUser.addEventListener('keyup', (event) => {
+	clearTimeout(timerId)
+	timerId = setTimeout(() => {
+		const user = event.target.value
+		if (user !== '') {
+			git
+				.getUser(user)
+				.then((data) => {
+					if (data.profile.message === 'Not Found') {
+						ui.notFound(user)
+					} else {
+						ui.updateUI(data.profile)
+					}
+				})
+				.catch((err) => console.log(err))
+			git
+				.getRepo(user)
+				.then((data) => {
+					ui.createRepo(data)
+				})
+				.catch((err) => console.log(err))
+		} else {
+			ui.clearUI()
+		}
+	}, 1000)
+})
